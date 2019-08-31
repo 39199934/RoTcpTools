20c41a616744cd1949cc4d91e2afed6e77c4b5f1
ro lodestar <ro@ros-MacBook-Air.local>
2019-08-31 20:10:44 +0800

    Initial Commit

---
+++ RoTcpTools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RoTcpTools
+//
+//  Created by ro lodestar on 2019/8/31.
+//  Copyright © 2019年 ro lodestar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RoTcpTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RoTcpTools
+//
+//  Created by ro lodestar on 2019/8/31.
+//  Copyright © 2019年 ro lodestar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
